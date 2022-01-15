7778546b7e8f0ac87805af0d9289e1081c46aadb
felipexbarbosa <43121492+felipexbarbosa@users.noreply.github.com>
2022-01-15 12:09:08 -0300

    basically removed 0exe's L code and made genes passing down but haven't done blood type.

--- docs/static/traits.js
+++ docs/static/traits.js
@@ -12,25 +12,6 @@ mutationRateSlider.addEventListener('change', function() {
     mutationRate = mutationRateSlider.value;
 });
 
-class Person {
-    constructor(genes) {
-        this.genes = {
-            'chromosome_1': [
-                genes['xy']['chromosome_1'],
-                genes['xx']['chromosome_1'],
-            ],
-            'chromosome_2': [
-                genes['xy']['chromosome_2'],
-                genes['xx']['chromosome_2'],
-            ],
-            'chromosome_3': [
-                genes['xy']['chromosome_3'],
-                genes['xx']['chromosome_3'],
-            ],
-        }
-    }
-}
-
 /* traits:
 
 for all the chromosomes, each parent will contribute 1 of each of their chromosomes
@@ -84,60 +65,33 @@ let freckles = ['true', 'false'];
 
 let sexes = ['XX', 'XY']
 
-function setExampleParent() {
-    let c1 = {
-        'xy': {
-            'p1': bloodTypes[Math.floor(Math.random() * bloodTypes.length)],
-            'q1': eyeColors[Math.floor(Math.random() * eyeColors.length)],
-            'q2': dimples[Math.floor(Math.random() * dimples.length)],
-        },
-        'xx': {
-            'p1': bloodTypes[Math.floor(Math.random() * bloodTypes.length)],
-            'q1': eyeColors[Math.floor(Math.random() * eyeColors.length)],
-            'q2': dimples[Math.floor(Math.random() * dimples.length)],
-        }
-    }
-
-    let c2 = {
-        'xy': {
-            'p1': hairColors[Math.floor(Math.random() * hairColors.length)],
-            'q1': freckles[Math.floor(Math.random() * freckles.length)],
-        },
-        'xx': {
-            'p1': hairColors[Math.floor(Math.random() * hairColors.length)],
-            'q1': freckles[Math.floor(Math.random() * freckles.length)],
-        },
-    }
-
-    let c3 = {
-        'xy': {
-            'sex': sexes[Math.floor(Math.random() * sexes.length)],
-        },
-        'xx': {
-            'sex': sexes[Math.floor(Math.random() * sexes.length)],
-        }
-    }
-
-    let personCreated = new Person(
-        {
-            'xx': {
-                'chromosome_1': c1['xx'],
-                'chromosome_2': c2['xx'],
-                'chromosome_3': c3['xx'],
-            },
-            'xy': {
-                'chromosome_1': c1['xy'],
-                'chromosome_2': c2['xy'],
-                'chromosome_3': c3['xy'],
-            },
-        },
-    )
-    
-    return personCreated;
-}
-
-let parent1 = setExampleParent();
-let parent2 = setExampleParent();
+let parent1 = {
+    "sex":["X","X"],
+    "hairColors": ["b","b"],
+    "dimples": ["D","D"],
+    "bloodTypes": ["A"],
+    "eyeColor": ["B","B"],
+    "freckles":["F","f"],
+};
+let parent2 = {
+    "sex":["X","Y"],
+    "hairColors": ["B","b"],
+    "dimples": ["D","D"],
+    "bloodTypes": ["AB"],
+    "eyeColor": ["g","g"],
+    "freckles":["f","f"],
+};
 
 console.log(parent1);
 console.log(parent2);
+function startGen() {
+    let offspring={
+    "sex": [parent1.sex[Math.floor(Math.random() * parent1.sex.length)],parent2.sex[Math.floor(Math.random() * parent2.sex.length)]],
+    "hairColor":[parent1.hairColors[Math.floor(Math.random() * parent1.hairColors.length)],parent2.hairColors[Math.floor(Math.random() * parent2.hairColors.length)]],
+    "freckles":[parent1.dimples[Math.floor(Math.random() * parent1.dimples.length)],parent2.dimples[Math.floor(Math.random() * parent2.dimples.length)]],
+    "eyeColor":[parent1.eyeColor[Math.floor(Math.random() * parent1.eyeColor.length)],parent2.eyeColor[Math.floor(Math.random() * parent2.eyeColor.length)]],
+    "freckles":[parent1.freckles[Math.floor(Math.random() * parent1.freckles.length)],parent2.freckles[Math.floor(Math.random() * parent2.freckles.length)]],
+    };
+    console.log(offspring);
+
+}
